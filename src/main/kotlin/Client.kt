12eb8c8518c5e7904d66394c1ce4f5aa8623e995
Dzefir5 <dzefir228@gmail.com>
2024-11-04 10:44:23 +0300

    Random client id

--- src/main/kotlin/Client.kt
+++ src/main/kotlin/Client.kt
@@ -1,8 +1,12 @@
 import java.util.concurrent.ConcurrentHashMap
 
 class ClientFactory(private val bank:Bank){
-    fun createClient(id: Int) : Client{
-        val client = Client(id , ConcurrentHashMap<String, Double>())
+    fun createClient(id: Int = (0..Int.MAX_VALUE).random()) : Client{
+        var clientId = id
+        while (bank.clients.containsKey(clientId)){
+            clientId =(0..Int.MAX_VALUE).random()
+        }
+        val client = Client(clientId , ConcurrentHashMap<String, Double>())
         bank.exchangeRates.forEach{ (currency, _) ->
             client.clientCurrency[currency]= 0.0
         }
